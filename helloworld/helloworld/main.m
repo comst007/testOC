ecefa34479ddb99c45209ef126ec4841a3a43ed7
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-09-11 04:15:32 +0800

    hello world

---
+++ helloworld/helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by comst on 15/9/11.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
