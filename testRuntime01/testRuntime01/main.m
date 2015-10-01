59e201c16180efafa37bbcb27f09f361a3d28e9d
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-02 05:48:27 +0800

    init

---
+++ testRuntime01/testRuntime01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testRuntime01
+//
+//  Created by comst on 15/10/2.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
