f9fbc244778dbeffc5679bd6476a321efae972bd
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-06 17:37:41 +0800

    runloop

--- LZThreadPro/LZThreadPro/ViewController.m
+++ LZThreadPro/LZThreadPro/ViewController.m
@@ -17,11 +17,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"main:%@", [NSThread currentThread]);
+    
+    NSThread *newthd = [[NSThread alloc] initWithTarget:self selector:@selector(thdRunloop) object:nil];
+    
+    [newthd start];
+    
+    [self performSelector:@selector(threadMain) onThread:newthd withObject:nil waitUntilDone:YES];
+    
+}
+/*
+ voidcreateCustomSource()
+ {
+ CFRunLoopSourceContext context = {0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+ CFRunLoopSourceRef source = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &context);
+ CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
+ while (pageStillLoading) {
+ NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+ CFRunLoopRun();
+ [pool release];
+ }
+ CFRunLoopRemoveSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
+ CFRelease(source);
+ }
+ */
+
+
+- (void)thdRunloop{
+    CFRunLoopSourceContext context = {0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+    CFRunLoopSourceRef source = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &context);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
+    CFRunLoopRun();
+    CFRunLoopRemoveSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
+    CFRelease(source);
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)threadMain{
+    [NSThread sleepForTimeInterval:3];
+    NSLog(@"%@", [NSThread currentThread]);
 }
+
+/**
+ *  creat new thread
+ */
+- (void)newThread1{
+    NSThread *newthread = [[NSThread alloc] initWithTarget:self selector:@selector(threadMain) object:nil];
+    NSLog(@"main:%@", [NSThread currentThread]);
+    
+    [newthread start];
+    
+    //    [[NSThread mainThre];
+    [NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:5]];
+}
+
+
+- (void)newThread2{
+ 
+    [self performSelectorInBackground:@selector(threadMain) withObject:nil];
+}
+
+- (void)newThread3{
+ 
+    [NSThread detachNewThreadSelector:@selector(threadMain) toTarget:self withObject:nil];
+}
+
 
 @end
