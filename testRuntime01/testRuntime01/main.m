5fb394e3f5f94daf9a5757524494ce24e25918de
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-06 13:57:40 +0800

    init

--- testRuntime01/testRuntime01/main.m
+++ testRuntime01/testRuntime01/main.m
@@ -24,6 +24,7 @@ static void dynamicResolveMethodIMP(id self, SEL _cmd){
 
 - (void)fastforwardMessage{
     NSLog(@"LZMouse %s", __func__);
+    
 }
 
 - (void)forwardMessage{
@@ -44,42 +45,43 @@ static void dynamicResolveMethodIMP(id self, SEL _cmd){
 @implementation LZCat
 
 - (void)normalMessage{
-     NSLog(@"%s", __func__);
+    NSLog(@"%s", __func__);
+
 }
 
 + (BOOL)resolveInstanceMethod:(SEL)sel{
-    
+
     NSString *selector = NSStringFromSelector(sel);
-    
+
     if ([selector isEqualToString:@"dynamicResolveMessage"]) {
         BOOL isOk ;
-        
-        isOk = class_addMethod([LZCat class], sel, (IMP)dynamicResolveMethodIMP, "v@:");
+
+    isOk               = class_addMethod([LZCat class], sel, (IMP)dynamicResolveMethodIMP, "v@:");
         if (!isOk) {
             NSLog(@"addmethod fail");
             return [super resolveInstanceMethod:sel];
         }
         return YES;
     }
-    
+
     return [super resolveInstanceMethod:sel];
 }
 
 - (id)forwardingTargetForSelector:(SEL)aSelector{
-    
+
     NSString *selector = NSStringFromSelector(aSelector);
-    
+
     if ([selector isEqualToString:@"fastforwardMessage"]) {
         return self.mouse;
     }
-    
+
     return [super forwardingTargetForSelector:aSelector];
 }
 
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation{
     NSString *selector = NSStringFromSelector(anInvocation.selector);
-    
+
     if ([selector isEqualToString:@"forwardMessage"]) {
         [anInvocation invokeWithTarget:self.mouse];
         return;
@@ -87,8 +89,9 @@ static void dynamicResolveMethodIMP(id self, SEL _cmd){
     [super forwardInvocation:anInvocation];
 }
 
+
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector{
-    
+
 
     return [NSMethodSignature signatureWithObjCTypes:"v@:"];
 
@@ -106,24 +109,24 @@ static void dynamicResolveMethodIMP(id self, SEL _cmd){
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-      
-        LZCat *acat = [[LZCat alloc] init];
-        
-        acat.mouse = [[LZMouse alloc] init];
+
+    LZCat *acat        = [[LZCat alloc] init];
+
+    acat.mouse         = [[LZMouse alloc] init];
         //正常的消息发送，在类的虚函数表了可以搜索得到
         [acat normalMessage];
-        
+
         //动态方法决议
         [acat dynamicResolveMessage];
-        
-        
+
+
         //  快速消息转发
         [acat fastforwardMessage];
-        
-        
+
+
         //正常消息转发,修改target
         [acat forwardMessage];
     }
-    
+
     return 0;
 }
