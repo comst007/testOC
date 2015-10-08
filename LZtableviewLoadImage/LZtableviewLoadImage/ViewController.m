905b0244d07b9230ffb95f9bc6907d561ab5e1e6
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-08 11:52:22 +0800

    init

---
+++ LZtableviewLoadImage/LZtableviewLoadImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LZtableviewLoadImage
+//
+//  Created by comst on 15/10/8.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
